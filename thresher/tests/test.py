10481eed3dc01484efa67a9881689c8dac188ec4
oskar-j <oskar.jarczyk@gmail.com>
2020-10-11 08:58:24 +0200

    Added 2 unit tests in test.py

--- thresher/tests/test.py
+++ thresher/tests/test.py
@@ -1,18 +1,41 @@
 import unittest
 import thresher
+from thresher import algorithm
+from thresher.oracle import run_oracle
+from thresher.tests.sample_data import get_sample_data
+
+
+class ThresherMediumTest(unittest.TestCase):
+
+    def setUp(self):
+        # Preparing data for unit test
+        self.t = thresher.Thresher(verbose=False, progress_bar=False)
+        medium_data = get_sample_data(path='./')
+        self.scores = list(medium_data['pred'].values)
+        self.actual_classes = list(medium_data['actual'].values)
+
+    def test_data_case(self):
+        compute_result = self.t.optimize_threshold(self.scores, self.actual_classes)
+        print(f'[ThresherMediumTest] Result found: {compute_result}')
+        self.assertTrue(0.4 <= compute_result < 0.6,
+                        msg="Checking proper result for the ThresherMediumTest")
+
+    def test_oracle(self):
+        self.assertTrue(run_oracle({'data_length': len(self.scores)}) == algorithm.available_algorithms['sgd'])
 
 
 class ThresherVerySmallTest(unittest.TestCase):
 
     def setUp(self):
         # Preparing data for unit test
-        self.t = thresher.Thresher()
+        self.t = thresher.Thresher(progress_bar=False)
 
     def test_data_case(self):
         scores = [0.1, 0.3, 0.4, 0.7]
         actual_classes = [-1, -1, 1, 1]
         compute_result = self.t.optimize_threshold(scores, actual_classes)
-        self.assertTrue(compute_result == 0.3,
+        print(f'[ThresherVerySmallTest] Result found: {compute_result}')
+        self.assertTrue(0.3 <= compute_result < 0.4,
                         msg="Checking proper result for the ThresherVerySmallTest")
 
     def test_options(self):
